24acabe69874efbae32206c78c0e2aa89aceb76e
yhoenyer21 <yhoenyeralvarado21@gmail.com>
2026-04-19 19:53:17 -0400

    Cambio Route::resource

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,6 @@ Route::get('/signup', function () {
     return view('signup');
 });
 
-Route::get('/especialidades', [EspecialidadController::class, 'index'])->name('especialidades.index');
-Route::get('/especialidades/create', [EspecialidadController::class, 'create'])->name('especialidades.create');
-Route::post('/especialidades', [EspecialidadController::class, 'store'])->name('especialidades.store');
-Route::get('/especialidades/{id}/edit', [EspecialidadController::class, 'edit'])->name('especialidades.edit');
-Route::put('/especialidades/{id}', [EspecialidadController::class, 'update'])->name('especialidades.update');
-Route::delete('/especialidades/{id}', [EspecialidadController::class, 'destroy'])->name('especialidades.destroy');
 
-Route::get('/medicos', [MedicoController::class, 'index'])->name('medicos.index');
-Route::get('/medicos/create', [MedicoController::class, 'create'])->name('medicos.create');
-Route::post('/medicos', [MedicoController::class, 'store'])->name('medicos.store');
-Route::get('/medicos/{id}/edit', [MedicoController::class, 'edit'])->name('medicos.edit');
-Route::put('/medicos/{id}', [MedicoController::class, 'update'])->name('medicos.update');
-Route::delete('/medicos/{id}', [MedicoController::class, 'destroy'])->name('medicos.destroy');
-
+Route::resource('especialidades', EspecialidadController::class);
+Route::resource('medicos', MedicoController::class);
