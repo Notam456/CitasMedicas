3b99134ac111ada1119ecbab5aed23ead0b2d90c
Notam456 <92233773+Notam456@users.noreply.github.com>
2026-05-30 01:15:10 -0400

    Seeder calendario ahora garantiza cupos el dia actual

--- database/seeders/CalendarioSeeder.php
+++ database/seeders/CalendarioSeeder.php
@@ -2,29 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Calendario;
 use App\Models\Medico;
+use Illuminate\Database\Seeder;
 
 class CalendarioSeeder extends Seeder
 {
     public function run()
     {
         $medicos = Medico::all();
-        $startDate = now()->startOfDay();
+        $startDate = now()->startOfDay()->subDay();
         $endDate = now()->addDays(90)->endOfDay();
+        $hoy = now()->format('Y-m-d');
 
-        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+        for ($date = $startDate->clone(); $date->lte($endDate); $date->addDay()) {
+
+            $fechaFormateada = $date->format('Y-m-d');
+            $isToday = ($fechaFormateada === $hoy);
+
             // No todos los médicos trabajan todos los días, solo algunos días laborales (lunes a viernes)
-            if ($date->isWeekend()) continue;
-
+            if ($date->isWeekend() && ! $isToday) {
+                continue;
+            }
             foreach ($medicos as $medico) {
                 // 60% de probabilidad de tener cupos ese día
-                if (rand(1, 100) <= 60) {
+                if ($isToday || rand(1, 100) <= 60) {
                     Calendario::updateOrCreate(
                         [
                             'medico_id' => $medico->id,
-                            'fecha' => $date->format('Y-m-d'),
+                            'fecha' => $fechaFormateada,
                         ],
                         [
                             'hora_inicio' => '08:00:00',
