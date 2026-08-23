59ae371302a984088fb8abfe722479bf69e3e249
jfyajure19 <jfyajure19@gmail.com>
2026-08-23 13:57:13 -0400

    modo oscuro con css nativo
    - modo oscuro hecho con css puro

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-light navbar-light">
-        <a href="{{route('dashboard')}}" class="navbar-brand mx-4 mb-3">
-            <h3 class="text-primary">SAGECIM</h3>
+        <a href="{{route('dashboard')}}" class="navbar-brand text-center w-100 d-block mb-3">
+            <h3 class="text-primary mb-0">SAGECIM</h3>
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
         </div>
